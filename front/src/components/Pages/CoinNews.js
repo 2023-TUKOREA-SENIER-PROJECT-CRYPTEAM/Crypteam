7d9c15dc7ca3400453ffbde5600ec6c2a67ec7b9
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-04-27 03:03:06 +0900

    [MODIFY]코인뉴스

--- front/src/components/Pages/CoinNews.js
+++ front/src/components/Pages/CoinNews.js
@@ -3,17 +3,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {  Box,Grid, Typography, TextField, Paper,Button,TableCell, TableContainer, Table, TableRow } from '@mui/material';
 //import NewsItem from '../NewsItem';
-
+// import axios from 'axios';
 function CoinNews() {
-  const [newsdata, setNewsData] = useState([]);
+  const [NewsData, setNewsData] = useState([{
+    id:'',
+    coin_name:'',
+    title:'',
+    url:'',
+    time:''
+  }]);
 
-  const [searchValue, setSearchValue] = useState('');
-  const [selectedCoin, setSelectedCoin] = useState('전체');
+  // const [searchValue, setSearchValue] = useState('');
+  // const [selectedCoin, setSelectedCoin] = useState('전체');
 
   useEffect(() => {
     async function fetchNewsData() {
-      const response = await axios.get('http://127.0.0.1:8000/api/news');
-      setNewsData(response.data);
+      const response = await axios.get('http://127.0.0.1:8000/api/news/');
+      setNewsData(JSON.parse(response.data));
     }
     fetchNewsData();
   }, []);
@@ -22,13 +28,13 @@ function CoinNews() {
   const handleCoinClick = (coin_name) => {
         setSelectedCoin(coin_name);
     };
-    const filteredCoins = newsdata.filter((newsdata) => {
-        if (selectedCoin === '전체') {
-            return newsdata.news_title.toLowerCase().includes(searchValue.toLowerCase());
-        } else {
-            return newsdata.coin_name === selectedCoin && newsdata.news_title.toLowerCase().includes(searchValue.toLowerCase());
-        }
-    });
+    // const filteredCoins = NewsData.filter((NewsData) => {
+    //     if (selectedCoin === '전체') {
+    //         return NewsData.news_title.toLowerCase().includes(searchValue.toLowerCase());
+    //     } else {
+    //         return NewsData.coin_name === selectedCoin && NewsData.news_title.toLowerCase().includes(searchValue.toLowerCase());
+    //     }
+    // });
 
 
 
@@ -58,14 +64,12 @@ function CoinNews() {
                 <TableCell sx={{ fontWeight: 'bold', borderBottomWidth: 2 }}>코인</TableCell>
               </TableRow>
 
-              {filteredCoins.map((newsdata,index) => (
-              <TableRow key={newsdata.id}>
-                  <TableCell component="th" scope="row" sx={{ borderBottomWidth: 2 }}>
-                  {index + 1}
-                  </TableCell>
-                  <TableCell onClick={() => window.open(newsdata.news_url, '_blank')} sx={{ borderBottomWidth: 2 }}>{newsdata.news_title}</TableCell>
-                  <TableCell sx={{ borderBottomWidth: 2 }}>{newsdata.news_registered_date}</TableCell>
-                  <TableCell sx={{ borderBottomWidth: 2 }}>{newsdata.coin_name}</TableCell>
+              {NewsData.map((NewsData) => (
+              <TableRow key={NewsData.id}>
+                  <TableCell sx={{ borderBottomWidth: 2 }}>{NewsData.id}</TableCell>
+                  <TableCell onClick={() => window.open(NewsData.url, '_blank')} sx={{ borderBottomWidth: 2 }}>{NewsData.title}</TableCell>
+                  <TableCell sx={{ borderBottomWidth: 2 }}>{NewsData.time}</TableCell>
+                  <TableCell sx={{ borderBottomWidth: 2 }}>{NewsData.coin_name}</TableCell>
               </TableRow>
               ))}
             </Table>
@@ -76,79 +80,3 @@ function CoinNews() {
 }
 
 export default CoinNews;
-
-
-// import React, {useState, useEffect} from 'react';
-// import axios from 'axios';
-// import {Link} from 'react-router-dom';
-// import {  Box,Grid, Typography, List, ListItem, ListItemText, ListItemSecondaryAction } from '@mui/material';
-
-// const CoinNews = () => {
-
-//     const[newsData, setNewsData] = useState([]);
-//     const NewsItem = ({id, title, coin, date, url})=>{
-//         const handleClick = () =>{
-//             window.open(url,'_blank');
-//         };
-//         return (
-//             <Typography key={id} sx={{ marginBottom: 1, cursor: 'pointer' }} onClick={handleClick}>
-//               [{coin}] {title} ({date})
-//             </Typography>
-//           );
-//     }
-//     const coins = ['전체', 'BTC', '이더리움', '거래소3', '거래소4'];
-//     const handleCoinClick = (coin) => {
-//         setSelectedCategory(coin);
-//       };
-
-//     useEffect(() =>{
-//         axios
-//             .get('')
-//             .then((response)=>{
-//                 setNewsData(response.newsdata);
-//             })
-//             .catch(error=>{
-//                 console.log(error);
-//             });
-//     },[]);
-
-    
-
-//     return(
-//         <>
-//         <Typography variant="h5" sx={{ marginBottom: 2 }}>
-//             거래소별 코인동향            
-//             <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            
-//                 {coins.map((coin) => (
-//                     <Typography
-//                     key={coin}
-//                     sx={{ marginRight: 1, marginBottom: 1, cursor: 'pointer' }}
-//                     onClick={() => handleCoinClick(coin)}
-//                     >
-//                     {coin}
-//                     </Typography>
-//                 ))}
-//             </Box>                        
-//         </Typography>
-        
-//         <List>        
-//         {newsData.map((news) => (
-//             <ListItem key={news.id} button component={Link} to={news.url}>
-//             <ListItemText
-//                 primary={news.title}
-//                 secondary={`${news.exchange} | ${news.date}`}
-//             />
-//             <ListItemSecondaryAction>
-//                 <Typography>{news.name}</Typography>
-//             </ListItemSecondaryAction>
-//             </ListItem>
-//         ))}
-//         </List>
-        
-//         </>
-
-//     )
-// };
-
-// export default CoinNews;
