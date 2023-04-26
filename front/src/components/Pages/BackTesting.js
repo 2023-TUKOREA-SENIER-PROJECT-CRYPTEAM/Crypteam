77751cd7dd83328e68456b0b0ac0ea75cf823d66
diddnjswns <diddnjswns20@gmail.com>
2023-04-27 00:57:41 +0900

    [design] 백테스팅 폼 변경중

--- front/src/components/Pages/BackTesting.js
+++ front/src/components/Pages/BackTesting.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import ParameterInput from "../function/parameter";
+import '../../css/Backtesting.css';
+
 
 const BackTesting = () => {
 
@@ -8,13 +9,12 @@ const BackTesting = () => {
         const [parameter, setParameter] = useState("");
         const [term, setTerm] = useState("");
         const [testSize, setTestSize] = useState("");
-        const [imgPath, setImgPath] = useState("");
         const [result, setResult] = useState("");
 
         const handleSubmit = (e) => {
             e.preventDefault(); //기본 폼 제출 방지
         
-            const formData = { coin_name: coinName, parameter, term, test_size: testSize, ImgPath: imgPath };
+            const formData = { coin_name: coinName, parameter, term, test_size: testSize};
             
             const url = 'http://127.0.0.1:8000/api/start_bot/';
             axios.post(url, formData) //백엔드에 post 요청
@@ -24,35 +24,33 @@ const BackTesting = () => {
 
     return(
         <>
-        <form onSubmit={handleSubmit}>
-            <div>
+        <form onSubmit={handleSubmit} className="form-container">
+            <div className="form-group">
                 <label htmlFor="coinName">Coin Name:</label>
                 <input type="text" id="coinName" value={coinName} onChange={(e) => setCoinName(e.target.value)} />
             </div>
-            <div>
+
+            <div className="form-group">
                 <label htmlFor="parameter">Parameter:</label>
                 <textarea id="parameter" value={parameter} onChange={(e) => setParameter(e.target.value)}></textarea>
-                {/*<ParameterInput></ParameterInput>*/}
             </div>
-            <div>
+            <div className="form-group">
                 <label htmlFor="term">Term:</label>
                 <input type="number" id="term" value={term} onChange={(e) => setTerm(e.target.value)} />
             </div>
-            <div>
+            <div className="form-group">
                 <label htmlFor="testSize">Test Size:</label>
                 <input type="number" id="testSize" value={testSize} onChange={(e) => setTestSize(e.target.value)} />
             </div>
+            <button type="submit" className="btn btn-primary">Start Bot</button>
             <div>
-                <label htmlFor="imgPath">Image Path:</label>
-                <input type="text" id="imgPath" value={imgPath} onChange={(e) => setImgPath(e.target.value)} />
-            </div>
-            <button type="submit">Start Bot</button>  
-            <div> 
-                <pre>{result}</pre>
+            <pre>{result}</pre>
             </div>
         </form>
-        </>
+</>
     )
 };
+
+
 
 export default BackTesting;
